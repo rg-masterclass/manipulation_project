285108957cb9ebcea3c1be5dbfa46dc41949a185
Robert Gruberski <rgruberski@gmail.com>
2023-12-28 07:56:30 +0000

    add protections

--- moveit2_scripts/src/pick_and_place.cpp
+++ moveit2_scripts/src/pick_and_place.cpp
@@ -14,6 +14,7 @@ int main(int argc, char **argv) {
       rclcpp::Node::make_shared("move_group_interface_tutorial", node_options);
 
   rclcpp::executors::SingleThreadedExecutor executor;
+
   executor.add_node(move_group_node);
   std::thread([&executor]() { executor.spin(); }).detach();
 
@@ -33,9 +34,9 @@ int main(int argc, char **argv) {
 
   // Get Current State
   moveit::core::RobotStatePtr current_state_arm =
-      move_group_arm.getCurrentState(10);
+      move_group_arm.getCurrentState(15);
   moveit::core::RobotStatePtr current_state_gripper =
-      move_group_gripper.getCurrentState(10);
+      move_group_gripper.getCurrentState(15);
 
   std::vector<double> joint_group_positions_arm;
   std::vector<double> joint_group_positions_gripper;
@@ -48,10 +49,9 @@ int main(int argc, char **argv) {
   move_group_gripper.setStartStateToCurrentState();
 
   moveit::planning_interface::MoveGroupInterface::Plan my_plan_arm;
-  bool success_arm = false;
-
   moveit::planning_interface::MoveGroupInterface::Plan my_plan_gripper;
-  bool success_gripper = false;
+
+  bool success = false;
 
   // Close Gripper
 
@@ -59,39 +59,31 @@ int main(int argc, char **argv) {
 
   move_group_gripper.setNamedTarget("gripper_close");
 
-  success_gripper = (move_group_gripper.plan(my_plan_gripper) ==
-                     moveit::core::MoveItErrorCode::SUCCESS);
+  success = (move_group_gripper.plan(my_plan_gripper) ==
+             moveit::core::MoveItErrorCode::SUCCESS);
+
+  if (!success) {
+    RCLCPP_ERROR(LOGGER, "I can't plan the trajectory to the "
+                         "'initial_gripper_close' position (planning error). I "
+                         "am trying to move the robot to the home position.");
+
+    move_group_arm.setNamedTarget("home");
+
+    success = (move_group_arm.plan(my_plan_arm) ==
+               moveit::core::MoveItErrorCode::SUCCESS);
+
+    if (success) {
+      move_group_arm.execute(my_plan_arm);
+    } else {
+      RCLCPP_ERROR(LOGGER,
+                   "I can't plan the trajectory to the 'home' position");
+    }
+
+    rclcpp::shutdown();
+    return -1;
+  }
 
   move_group_gripper.execute(my_plan_gripper);
-
-  // Home
-  //   RCLCPP_INFO(LOGGER, "Home Position");
-
-  //   geometry_msgs::msg::Pose home_pose;
-  //   home_pose.orientation.x = 0.635;
-  //   home_pose.orientation.y = -0.629;
-  //   home_pose.orientation.z = 0.310;
-  //   home_pose.orientation.w = -0.323;
-  //   home_pose.position.x = 0.045;
-  //   home_pose.position.y = 0.133;
-  //   home_pose.position.z = 0.490;
-
-  //   move_group_arm.setPoseTarget(home_pose);
-
-  //   success_arm = (move_group_arm.plan(my_plan_arm) ==
-  //                  moveit::core::MoveItErrorCode::SUCCESS);
-
-  //   move_group_arm.execute(my_plan_arm);
-
-  // - Translation: [0.344, -0.020, 0.244]
-  // - Rotation: in Quaternion [0.735, -0.678, 0.004, -0.017]
-  // - Rotation: in RPY (radian) [-3.111, 0.018, -1.489]
-  // - Rotation: in RPY (degree) [-178.257, 1.051, -85.332]
-  // - Matrix:
-  // 0.081 -0.996  0.029  0.344
-  // -0.997 -0.081  0.021 -0.020
-  // -0.018 -0.030 -0.999  0.244
-  // 0.000  0.000  0.000  1.000
 
   // Pregrasp
   RCLCPP_INFO(LOGGER, "Pregrasp Position");
@@ -103,11 +95,33 @@ int main(int argc, char **argv) {
   target_pose1.orientation.w = 0.000;
   target_pose1.position.x = 0.343; // sim: 0.34;
   target_pose1.position.y = 0.132; // sim: -0.02;
-  target_pose1.position.z = 0.264;
+  target_pose1.position.z = 0.27;
   move_group_arm.setPoseTarget(target_pose1);
 
-  success_arm = (move_group_arm.plan(my_plan_arm) ==
-                 moveit::core::MoveItErrorCode::SUCCESS);
+  success = (move_group_arm.plan(my_plan_arm) ==
+             moveit::core::MoveItErrorCode::SUCCESS);
+
+  if (!success) {
+    RCLCPP_ERROR(
+        LOGGER,
+        "I can't plan the trajectory to the 'pregrasp' position (planning "
+        "error). I am trying to move the robot to the home position.");
+
+    move_group_arm.setNamedTarget("home");
+
+    success = (move_group_arm.plan(my_plan_arm) ==
+               moveit::core::MoveItErrorCode::SUCCESS);
+
+    if (success) {
+      move_group_arm.execute(my_plan_arm);
+    } else {
+      RCLCPP_ERROR(LOGGER,
+                   "I can't plan the trajectory to the 'home' position");
+    }
+
+    rclcpp::shutdown();
+    return -1;
+  }
 
   move_group_arm.execute(my_plan_arm);
 
@@ -117,8 +131,29 @@ int main(int argc, char **argv) {
 
   move_group_gripper.setNamedTarget("gripper_open");
 
-  success_gripper = (move_group_gripper.plan(my_plan_gripper) ==
-                     moveit::core::MoveItErrorCode::SUCCESS);
+  success = (move_group_gripper.plan(my_plan_gripper) ==
+             moveit::core::MoveItErrorCode::SUCCESS);
+
+  if (!success) {
+    RCLCPP_ERROR(LOGGER, "I can't plan the trajectory to the "
+                         "'object_gripper_open' position (planning error). I "
+                         "am trying to move the robot to the home position.");
+
+    move_group_arm.setNamedTarget("home");
+
+    success = (move_group_arm.plan(my_plan_arm) ==
+               moveit::core::MoveItErrorCode::SUCCESS);
+
+    if (success) {
+      move_group_arm.execute(my_plan_arm);
+    } else {
+      RCLCPP_ERROR(LOGGER,
+                   "I can't plan the trajectory to the 'home' position");
+    }
+
+    rclcpp::shutdown();
+    return -1;
+  }
 
   move_group_gripper.execute(my_plan_gripper);
 
@@ -148,14 +183,34 @@ int main(int argc, char **argv) {
 
   move_group_gripper.setNamedTarget("gripper_close");
 
-  success_gripper = (move_group_gripper.plan(my_plan_gripper) ==
-                     moveit::core::MoveItErrorCode::SUCCESS);
+  success = (move_group_gripper.plan(my_plan_gripper) ==
+             moveit::core::MoveItErrorCode::SUCCESS);
+
+  if (!success) {
+    RCLCPP_ERROR(LOGGER, "I can't plan the trajectory to the "
+                         "'object_gripper_close' position (planning error). I "
+                         "am trying to move the robot to the home position.");
+
+    move_group_arm.setNamedTarget("home");
+
+    success = (move_group_arm.plan(my_plan_arm) ==
+               moveit::core::MoveItErrorCode::SUCCESS);
+
+    if (success) {
+      move_group_arm.execute(my_plan_arm);
+    } else {
+      RCLCPP_ERROR(LOGGER,
+                   "I can't plan the trajectory to the 'home' position");
+    }
+
+    rclcpp::shutdown();
+    return -1;
+  }
 
   move_group_gripper.execute(my_plan_gripper);
 
   // Retreat
-
-  //   RCLCPP_INFO(LOGGER, "Retreat from object!");
+  RCLCPP_INFO(LOGGER, "Retreat from object!");
 
   std::this_thread::sleep_for(std::chrono::seconds(1));
 
@@ -186,19 +241,61 @@ int main(int argc, char **argv) {
   target_pose1.position.z = 0.284;
   move_group_arm.setPoseTarget(target_pose1);
 
-  success_arm = (move_group_arm.plan(my_plan_arm) ==
-                 moveit::core::MoveItErrorCode::SUCCESS);
+  success = (move_group_arm.plan(my_plan_arm) ==
+             moveit::core::MoveItErrorCode::SUCCESS);
+
+  if (!success) {
+    RCLCPP_ERROR(
+        LOGGER,
+        "I can't plan the trajectory to the 'turnover' position (planning "
+        "error). I am trying to move the robot to the home position.");
+
+    move_group_arm.setNamedTarget("home");
+
+    success = (move_group_arm.plan(my_plan_arm) ==
+               moveit::core::MoveItErrorCode::SUCCESS);
+
+    if (success) {
+      move_group_arm.execute(my_plan_arm);
+    } else {
+      RCLCPP_ERROR(LOGGER,
+                   "I can't plan the trajectory to the 'home' position");
+    }
+
+    rclcpp::shutdown();
+    return -1;
+  }
 
   move_group_arm.execute(my_plan_arm);
 
   // Open Gripper
-
   RCLCPP_INFO(LOGGER, "Open Gripper!");
 
   move_group_gripper.setNamedTarget("gripper_open");
 
-  success_gripper = (move_group_gripper.plan(my_plan_gripper) ==
-                     moveit::core::MoveItErrorCode::SUCCESS);
+  success = (move_group_gripper.plan(my_plan_gripper) ==
+             moveit::core::MoveItErrorCode::SUCCESS);
+
+  if (!success) {
+    RCLCPP_ERROR(LOGGER, "I can't plan the trajectory to the "
+                         "'finaly_gripper_open' position (planning error). I "
+                         "am trying to move the robot to the home position.");
+
+    move_group_arm.setNamedTarget("home");
+
+    success = (move_group_arm.plan(my_plan_arm) ==
+               moveit::core::MoveItErrorCode::SUCCESS);
+
+    if (success) {
+      move_group_arm.execute(my_plan_arm);
+    } else {
+      RCLCPP_ERROR(LOGGER,
+                   "I can't plan the trajectory to the 'home' position");
+    }
+
+    rclcpp::shutdown();
+    return -1;
+  }
 
   move_group_gripper.execute(my_plan_gripper);
 
